adc23d402849cb802f0e881a0f2aacfbdcb3b621
Zhaoning Zhang <42299684+ZhangZhaoning@users.noreply.github.com>
2024-12-20 20:58:38 -0600

    Update train_classifier.py

--- Disaster_Pipeline/models/train_classifier.py
+++ Disaster_Pipeline/models/train_classifier.py
@@ -1,24 +1,82 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.externals import joblib
+from sklearn.model_selection import RandomizedSearchCV
+import pickle
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///'+ database_filepath)
+    df = pd.read_sql_table('messages',engine)
+    
+    # define features and target
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    text = re.sub(r'[^a-zA-Z0-9]', ' ', text.lower())
+    words = word_tokenize(text)
+    words = [w for w in words if w not in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(w) for w in words]
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))
+])
+    parameters = {
+    'clf__estimator__n_estimators': [50],
+    #'clf__estimator__max_depth': [10, 20, None],
+    'clf__estimator__min_samples_split': [2]
+}
+
+    # Use RandomizedSearchCV
+    cv = RandomizedSearchCV(
+        pipeline,
+        param_distributions=parameters,
+        n_iter=1,               
+        verbose=2,
+        n_jobs=-1,
+        random_state=42          
+    )
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    for i, column in enumerate(category_names):
+        print(f'Category: {column}\n')
+        print(classification_report(Y_test.iloc[:, i], Y_pred[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
