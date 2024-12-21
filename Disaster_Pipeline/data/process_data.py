5f559e44e38cbae07cce64e98f3be7bcc02c25bc
Zhaoning Zhang <42299684+ZhangZhaoning@users.noreply.github.com>
2024-12-20 20:57:39 -0600

    Update process_data.py

--- Disaster_Pipeline/data/process_data.py
+++ Disaster_Pipeline/data/process_data.py
@@ -1,16 +1,35 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = messages.merge(categories, how='inner', on= 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+    # Split the categories into separate columns
+    categories = df['categories'].str.split(';', expand=True)
+    # Use the first row to extract category column names
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2]).tolist()
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+    df = df.drop('categories', axis=1)
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('messages', engine, index=False) 
 
 
 def main():
